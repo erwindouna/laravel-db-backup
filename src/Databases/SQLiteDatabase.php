c6e4dbb2e8f5419cdfeff103bdf4ee821d246598
Erwin Douna <e.douna@gmail.com>
2020-05-24 22:16:30 +0200

    Adding stub methods

--- src/Databases/SQLiteDatabase.php
+++ src/Databases/SQLiteDatabase.php
@@ -1,1 +1,25 @@
 <?php
+
+namespace EDouna\LaravelDBBackup\Databases;
+
+class SQLiteDatabase implements DatabaseInterface
+{
+
+    public function backup()
+    {
+        // TODO: Implement backup() method.
+    }
+
+    public function restore(string $backupFile)
+    {
+        // TODO: Implement restore() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFileExtension()
+    {
+        // TODO: Implement getFileExtension() method.
+    }
+}
